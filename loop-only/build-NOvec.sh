0ef40a581856b27d20563d5358844a52a85a7eb7
stevehudson <shuds13@gmail.com>
2016-07-25 17:41:29 +0100

    Fix build scripts

--- loop-only/build-NOvec.sh
+++ loop-only/build-NOvec.sh
@@ -1,5 +1,5 @@
 #TRY 02
-icc -O2 --no-vec -o test vector123.c
+icc -O2 -qopenmp -no-vec -o test-novec.x vector123.c
 
 #TRY 03
 #iCC -O3 --no-vec -o test vector123.c
--- loop-only/build-vec.sh
+++ loop-only/build-vec.sh
@@ -5,7 +5,7 @@
 #iCC -O3 -xhost -o test vector123.c
 
 #KNL 02 - PROB NO DIFF THAN XHOST
-icc -O2 -xMIC-AVX512 -o test vector123.c
+icc -O2 -qopenmp -xMIC-AVX512 -o test-vec.x vector123.c
 
 #KNL 03 - PROB NO DIFF THAN XHOST
 #iCC -O3 -xMIC-AVX512 -o test vector123.c
